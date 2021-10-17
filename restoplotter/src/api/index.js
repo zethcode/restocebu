48b6d6259798da6ad8837bd863f8fcedd87569b2
zethcode <89132475+zethcode@users.noreply.github.com>
2021-10-18 05:13:34 +0800

    Updated url from get restaurants axios request

--- restoplotter/src/api/index.js
+++ restoplotter/src/api/index.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export const getRestaurants = async () => {
-    return await axios.get('./db/restaurant.json')
+    return await axios.get(window.location.href + '/db/restaurant.json')
         .then((res) => {
+            console.log("not printing", window.location.href)
             return [...res.data];
         }).catch((err) => {
+            console.log("not printing", window.location.href)
             return {};
         })
 }
